97c75da8843981cf6b2ee1ddf1c01a02fb4c2662
BaeMinChoen <baemincheon@gmail.com>
2018-01-13 19:54:51 +0900

    v6.0

---
+++ script.js
@@ -0,0 +1,25 @@
+// TItle
+var strTitle = "UnlightSC Guide KOR";
+document.title = strTitle;
+
+// Messages
+var strMsg01 = "가로 해상도 750px 이상의 환경을 권장합니다.";
+
+// HOME and MENUs
+var strHome = "HOME";
+var strMenu01 = "MENU#1";
+var strMenu02 = "MENU#2";
+var strMenu03 = "MENU#3";
+
+// Document 001~099
+var strM001 = "INFO";
+var strM002 = "HELP"
+
+// Document 101~199
+var strM101 = "M101";
+
+// Document 201~299
+var strM201 = "M201";
+
+// Document 301~399
+var strM301 = "M301";
