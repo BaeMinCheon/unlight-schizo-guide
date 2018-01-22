e8b8f4da543c982604d0d1dd77f9b4b0d40bf9a4
BaeMinChoen <baemincheon@gmail.com>
2018-01-22 22:32:57 +0900

    v7.2

--- script.js
+++ script.js
@@ -1,26 +1,126 @@
 // TItle
-var strTitle = "UnlightSC Guide KOR";
-document.title = strTitle;
+document.title = "UnlightSC Guide KOR";
 
 // Messages
 var strMsg01 = "가로 해상도 900px 이상의 환경을 권장합니다.";
 
 // HOME and MENUs
 var strHome = "HOME";
+var strToHome01 =
+  '<a href="index.html"> \
+    <h1>' +
+      strHome +
+  '  </h1> \
+  </a>';
+var strToHome02 =
+  '<a href="../index.html"> \
+    <h1>' +
+      strHome +
+  '  </h1> \
+  </a>';
+
 var strMenu01 = "Basic";
 var strMenu02 = "Advanced";
 var strMenu03 = "Reference";
+var strToMenu01 =
+  '<a href="MENU01/M100.html"> \
+    <h2>' +
+      strMenu01 +
+    '</h2> \
+  </a> \
+  <a href="MENU02/M200.html"> \
+    <h2>' +
+        strMenu02 +
+    '</h2> \
+  </a> \
+  <a href="MENU03/M300.html"> \
+    <h2>' +
+        strMenu03 +
+    '</h2> \
+  </a>';
+var strToMenu02 =
+  '<a href="../MENU01/M100.html"> \
+    <h2>' +
+      strMenu01 +
+    '</h2> \
+  </a> \
+  <a href="../MENU02/M200.html"> \
+    <h2>' +
+        strMenu02 +
+    '</h2> \
+  </a> \
+  <a href="../MENU03/M300.html"> \
+    <h2>' +
+        strMenu03 +
+    '</h2> \
+  </a>';
+
+// Common Codes
+var strCommon01 =
+  '<div id="NOTICE">' +
+    strMsg01 +
+  '</div> \
+  <div id="HOME">' +
+    strToHome01 +
+  '</div> \
+  <div id="MENU">' +
+    strToMenu01 +
+  '</div>';
+var strCommon02 =
+  '<div id="NOTICE">' +
+    strMsg01 +
+  '</div> \
+  <div id="HOME">' +
+    strToHome02 +
+  '</div> \
+  <div id="MENU">' +
+    strToMenu02 +
+  '</div>';
 
 // Document 001~099
 var strM001 = "INFO";
 var strM002 = "HELP"
+var strSub01 =
+  '<a href="M001.html"> \
+    <h3>' +
+      strM001 +
+  '  </h3> \
+  </a> \
+  <a href="M002.html"> \
+    <h3>' +
+      strM002 +
+  '  </h3> \
+  </a>';
 
 // Document 101~199
 var strM101 = "튜토리얼";
-var strM102 = "다크룸";
+var strM102 = "FAQ & RULE";
+var strSub02 =
+  '<a href="M101.html"> \
+    <h3>' +
+      strM101 +
+  '  </h3> \
+  </a> \
+  <a href="M102.html"> \
+    <h3>' +
+      strM102 +
+  '  </h3> \
+  </a>';
 
 // Document 201~299
 var strM201 = "덱 구성";
+var strSub03 =
+  '<a href="M201.html"> \
+    <h3>' +
+      strM201 +
+  '  </h3> \
+  </a>';
 
 // Document 301~399
 var strM301 = "동영상 가이드";
+var strSub04 =
+  '<a href="M301.html"> \
+    <h3>' +
+      strM301 +
+  '  </h3> \
+  </a>';
